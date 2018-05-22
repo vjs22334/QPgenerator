6a7caaa4b6acd2f7a5b80b6b282eb6fd2aa03d3a
vjs2234 <jeyasurya.v@gmail.com>
2018-05-22 17:12:17 +0530

    created add questions and add choices view

--- QPgenerator/qp_generator/views.py
+++ QPgenerator/qp_generator/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . import forms
+from . import models
 from .decorator import user_passes_test_message,login_required_message
+from django.forms import modelformset_factory,inlineformset_factory
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -38,7 +40,32 @@ def menu(request):
 @login_required_message
 @user_passes_test_message(lambda u : u.profile.role == 'admin',message="not authorized")
 def add_questions(request):
-    return HttpResponse("under construction")
-
-def permission_denied(request):
-    return render(request,'permission_denied.html')        
+    if request.method == 'POST':
+        question_form = forms.QuestionForm(request.POST)
+        if question_form.is_valid():
+            question_form.save()
+            question_type = question_form.cleaned_data.get('question_type')
+            question_id = question_form.cleaned_data.get('id')
+            if question_type == 'mcq' or question_type == 'fb':
+                return redirect('add_choices',question_id=question_id)
+            else:
+                return redirect('menu')
+    else:
+        question_form = forms.QuestionForm()
+        return render(request,'add_question.html',{
+            "question_form" : question_form
+        })
+def add_choices(request,question_id):
+    question = models.Question.objects.get(id=question_id)
+    ChoiceFormset = inlineformset_factory(models.Question,models.Choice,fields=("choice_text",))
+    formset = ChoiceFormset(instance=question)
+    if request.method == 'POST':
+        formset = ChoiceFormset(request.Post,request.FILES,instance=question)
+        if formset.is_valid():
+            formset.save()
+            return redirect('menu')
+    else:
+        formset = ChoiceFormset(instance=question)
+        return redirect(request,'add_choices.html',{
+            "formset" : formset
+        })
